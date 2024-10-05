1509a0d9b6fe81abc3265c063ebf9a722eb79fcc
nahareshan05@gmail.com <nahareshan05@gmail.com>
2024-10-05 11:40:28 +0530

    Many changes for React and React Components
    - Added introduction under React
    - Add various components and their overview and component code and usage
    - Add codes under codes/ for better organization

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,11 +22,33 @@ export default defineConfig({
         },
         {
           label: "React",
-          items: [{ label: "Introduction", slug: "react/introduction" }],
+          items: [
+            { label: "Introduction", slug: "react/introduction" },
+            { label: "Components", slug: "react/components" }
+          ],
         },
         {
           label: "React Components",
-          items: [{ label: "Buttons", slug: "react-components/buttons" }],
+          items: [
+            { label: "Accordion", slug: "react-components/accordion" },
+            { label: "Alerts", slug: "react-components/alert" },
+            { label: "Breadcrumb", slug: "react-components/breadcrumb" },
+            { label: "Button Group", slug: "react-components/button-group" },
+            { label: "Buttons", slug: "react-components/buttons" },
+            { label: "Chips", slug: "react-components/chips" },
+            { label: "Dropdowns", slug: "react-components/dropdown" },
+            { label: "Input", slug: "react-components/input" },
+            { label: "Lightbox", slug: "react-components/lightbox" },
+            { label: "Modal", slug: "react-components/modal" },
+            { label: "Navigation Bar", slug: "react-components/navbar" },
+            { label: "Popovers", slug: "react-components/popover" },
+            { label: "Social Buttons", slug: "react-components/social-buttons" },
+            { label: "Star Rating", slug: "react-components/star-rating" },
+            { label: "Tabs", slug: "react-components/tabs" },
+            { label: "Testimonials", slug: "react-components/testimonials" },
+            { label: "Toast", slug: "react-components/toast" },
+            { label: "Tooltip", slug: "react-components/tooltip" },
+          ],
         },
         {
           label: "Node",
